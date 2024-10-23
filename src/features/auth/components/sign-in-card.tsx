28fe21202bbdd36809208ec67b667696333d7020
Anshu Sah <anshu7sah@gmail.com>
2024-10-23 11:01:46 +0545

    completed signin page

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,4 +1,9 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 export const SignInCard = () => {
   return (
@@ -6,6 +11,46 @@ export const SignInCard = () => {
       <CardHeader className="flex items-center justify-center text-center p-7">
         <CardTitle className="text-2xl">Welcome back!</CardTitle>
       </CardHeader>
+      <div className="p-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <form className="space-y-4">
+          <Input
+            required
+            type="email"
+            value={""}
+            placeholder="Enter email address"
+            onChange={() => {}}
+            disabled={false}
+          />
+          <Input
+            required
+            type="password"
+            placeholder="Enter your password"
+            onChange={() => {}}
+            min={8}
+            max={256}
+            disabled={false}
+          />
+          <Button className="w-full" disabled={false} size={"lg"}>
+            Login
+          </Button>
+        </form>
+      </CardContent>
+      <div className="p-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="flex flex-col gap-y-4 p-7">
+        <Button size={"lg"} variant="secondary" disabled={false}>
+          <FcGoogle className="mr-2" size={5} />
+          Login with Google
+        </Button>
+        <Button size={"lg"} variant="secondary" disabled={false}>
+          <FaGithub className="mr-2 size-5" />
+          Login with Github
+        </Button>
+      </CardContent>
     </Card>
   );
 };
