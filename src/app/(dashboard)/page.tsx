35e5743118977bc404a499a6b68e61fc87ff4b42
Anshu Sah <anshu7sah@gmail.com>
2024-10-26 23:23:31 +0545

    completed sidebar navigation bar. Both sidebar and navigation is mobile responsive

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,12 +1,10 @@
 import { getCurrent } from "@/features/auth/actions";
 import { UserButton } from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
-export default async function Home() {
+async function Home() {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
-  return (
-    <div className="">
-      <UserButton />
-    </div>
-  );
+  return <div className="">This is pag</div>;
 }
+
+export default Home;
