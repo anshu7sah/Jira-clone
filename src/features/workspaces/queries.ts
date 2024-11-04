f073fea6acfd19af8759811e49134acc49d97ece
Anshu Sah <anshu7sah@gmail.com>
2024-11-05 01:15:16 +0545

    refactored the redundant code

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,23 +1,12 @@
-import { cookies } from "next/headers";
-import { Account, Client, Databases, Query } from "node-appwrite";
-import { AUTH_COOKIE } from "../auth/constants";
+import { Query } from "node-appwrite";
 import { DATABASE_ID, MEMBER_ID, WORKSPACE_ID } from "@/config";
 import { getMembers } from "../members/utils";
 import { Workspace } from "./types";
+import { createSessionClient } from "@/lib/appwrite";
 
 export const getWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-    const session = await cookies().get(AUTH_COOKIE);
-    if (!session) {
-      return { documents: [], total: 0 };
-    }
-    client.setSession(session.value);
-    const account = new Account(client);
-    const databases = new Databases(client);
+    const { account, databases } = await createSessionClient();
     const user = await account.get();
 
     const members = await databases.listDocuments(DATABASE_ID, MEMBER_ID, [
@@ -47,17 +36,8 @@ interface WorkspaceId {
 
 export const getWorkspace = async ({ workspaceId }: WorkspaceId) => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+    const { account, databases } = await createSessionClient();
 
-    const session = await cookies().get(AUTH_COOKIE);
-    if (!session) {
-      return null;
-    }
-    client.setSession(session.value);
-    const account = new Account(client);
-    const databases = new Databases(client);
     const user = await account.get();
 
     const members = await getMembers({
